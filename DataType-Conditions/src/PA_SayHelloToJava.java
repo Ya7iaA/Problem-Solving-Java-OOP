158dd355fe96961ee421bfaf705d0e3bc39d657f
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-20 20:12:26 +0300

    Say Hello With Java solved

--- DataType-Conditions/src/PA_SayHelloToJava.java
+++ DataType-Conditions/src/PA_SayHelloToJava.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class PA_SayHelloToJava {
+    Scanner s = new Scanner(System.in);
+
+    private String inputName;
+
+    public PA_SayHelloToJava() {
+        inputName = s.nextLine();
+    }
+
+    public void run() {
+        System.out.println("Hello, " + inputName);
+    }
+
     public static void main(String[] args) {
-        
+        PA_SayHelloToJava sayHelloToJava = new PA_SayHelloToJava();
+        sayHelloToJava.run();
     }
 }
