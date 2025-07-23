b1605561430680c707de696b031c0113074e04e3
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-23 18:45:42 +0300

    First Practice done

--- Mini-OOP/Practice/PracticeOne/src/App.java
+++ Mini-OOP/Practice/PracticeOne/src/App.java
@@ -11,6 +11,22 @@ public class App {
         personAge = s.nextInt();
     }
 
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonAge(int personAge) {
+        this.personAge = personAge;
+    }
+
+    public int getPersonAge() {
+        return personAge;
+    }
+
     public void print() {
         System.out.println("Person Name : " + personName);
         System.out.println("Person Age : " + personAge);
