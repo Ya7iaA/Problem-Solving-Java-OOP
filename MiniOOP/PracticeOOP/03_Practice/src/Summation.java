3c2a09546b3a8e25002a3a52f8413409c117ef91
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-23 22:16:05 +0300

    Third Practice done

--- MiniOOP/PracticeOOP/03_Practice/src/Summation.java
+++ MiniOOP/PracticeOOP/03_Practice/src/Summation.java
@@ -13,31 +13,7 @@ public class Summation {
             this.thirdNumber = thirdNumber;
         }
     }
-
-    public void setFirstNumber(int firstNumber) {
-        this.firstNumber = firstNumber;
-    }
-
-    public int getFirstNumber() {
-        return firstNumber;
-    }
-
-    public void setSecondNumber(int secondNumber) {
-        this.secondNumber = secondNumber;
-    }
-
-    public int getSecondNumber() {
-        return secondNumber;
-    }
-
-    public void setThirdNumber(int thirdNumber) {
-        this.thirdNumber = thirdNumber;
-    }
-
-    public int getThirdNumber() {
-        return thirdNumber;
-    }
-
+    
     public int theSum() {
         if (firstNumber % 2 != 0 || secondNumber % 2 != 0 || thirdNumber % 2 != 0) {
             return -1;
