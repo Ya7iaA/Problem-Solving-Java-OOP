bbf7ce4c5ebb1bc17d76b3c2a5a805fc62ce63ab
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-23 22:10:42 +0300

    First Practice done

--- MiniOOP/PracticeOOP/01_Practice/src/App.java
+++ MiniOOP/PracticeOOP/01_Practice/src/App.java
@@ -8,22 +8,6 @@ public class App {
         this.personAge = personAge;
     }
 
-    public void setPersonName(String personName) {
-        this.personName = personName;
-    }
-
-    public String getPersonName() {
-        return personName;
-    }
-
-    public void setPersonAge(int personAge) {
-        this.personAge = personAge;
-    }
-
-    public int getPersonAge() {
-        return personAge;
-    }
-
     public void print() {
         System.out.println("Person Name : " + personName);
         System.out.println("Person Age : " + personAge);
