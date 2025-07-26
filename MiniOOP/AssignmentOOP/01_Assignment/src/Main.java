9f38abbdc5f57ce651e713af8519d8e726b7d2bb
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-07-26 15:15:24 +0300

    First Assignment done

--- MiniOOP/AssignmentOOP/01_Assignment/src/Main.java
+++ MiniOOP/AssignmentOOP/01_Assignment/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         Employee employee = new Employee(s.nextLine(), s.nextInt(), s.nextDouble(), s.nextLine());
         employee.work();
         System.out.println("---------------------------");
-        Chef chef = new Chef(s.nextLine(), s.nextInt(), s.nextDouble();
+        Chef chef = new Chef(s.nextLine(), s.nextInt(), s.nextDouble());
         chef.work();
         System.out.println("---------------------------");
         Waiter waiter = new Waiter(s.nextLine(), s.nextInt(), s.nextDouble());
